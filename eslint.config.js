0bf28c8dd8df0c50545390640d03824559804281
Luis Montes <monteslu@gmail.com>
2026-02-03 16:24:11 -0700

    Add ESLint config

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+import js from '@eslint/js';
+
+export default [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      globals: {
+        fetch: 'readonly',
+        AbortController: 'readonly',
+        setTimeout: 'readonly',
+        clearTimeout: 'readonly',
+        URL: 'readonly',
+        console: 'readonly'
+      }
+    },
+    rules: {
+      'no-unused-vars': ['error', { argsIgnorePattern: '^_' }]
+    }
+  }
+];
